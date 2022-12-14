eca7054124697b88821878e51f37c63e03599201
z vallarino <zvcoding@gmail.com>
2022-12-14 15:08:11 -0500

    working

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,10 +1,21 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import tailwind from 'twrnc'
 import ModalDropdown from 'react-native-modal-dropdown'
+import CheckBox from '../components/Checkbox'
+
 // import ModalDropdown from 'react-native-modal-dropdown';
 
 const FilterScreen = () => {
+
+  const [checked,setChecked] = useState(false)
+
+  const arrayExample = ['one','two','three']
+
+  const mapOfArray = () => {
+    
+  }
+  
   return (
     <View style = {tailwind`bg-blue-200 h-full`}>
       <Text>Legend</Text>
@@ -20,7 +31,9 @@ const FilterScreen = () => {
       <Text>Travel</Text>
       <View>
         <Text>Air Travel</Text>
+          <CheckBox status = {'checked'} label = {"Next"} onPress = {(e)=>console.log('hello')} />
       </View>
+    
       <TouchableOpacity style={tailwind`px-6 py-2.5 bg-blue-600 leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out`}>
         <Text style = {tailwind`text-white font-medium text-md`}>Apply Filter</Text>
       </TouchableOpacity>
